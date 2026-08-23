55067e2629cf0d3de749cda8be3bb7fbdad23098
Certifiedcook <beanarestalin@gmail.com>
2026-08-23 23:14:00 +0100

    Expand overlay and ore settings

--- src/main/java/dev/cd/diagnostics/DiagnosticsSettings.java
+++ src/main/java/dev/cd/diagnostics/DiagnosticsSettings.java
@@ -2,10 +2,33 @@ package dev.cd.diagnostics;
 
 public final class DiagnosticsSettings {
     public static boolean playerEsp = true;
+    public static boolean playerTracers = false;
+
     public static boolean storageEsp = true;
-    public static boolean tracers = false;
-    public static boolean depthOverride = true;
+    public static boolean storageDepthOverride = true;
+    public static boolean showChests = true;
+    public static boolean showBarrels = true;
+    public static boolean showShulkers = true;
+    public static boolean showEnderChests = true;
+
+    public static boolean oreEsp = true;
+    public static boolean oreDepthOverride = true;
+    public static boolean showDiamond = true;
+    public static boolean showAncientDebris = true;
+    public static boolean showIron = true;
+    public static boolean showGold = true;
+    public static boolean showLapis = true;
+    public static boolean showRedstone = true;
 
     private DiagnosticsSettings() {
+    }
+
+    public static boolean anyOreEnabled() {
+        return showDiamond
+                || showAncientDebris
+                || showIron
+                || showGold
+                || showLapis
+                || showRedstone;
     }
 }
