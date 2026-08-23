f9a4085d3daed1179938b5943ba4a769a14206e1
Certifiedcook <beanarestalin@gmail.com>
2026-08-23 22:35:34 +0100

    Add diagnostics settings

---
+++ src/main/java/dev/cd/diagnostics/DiagnosticsSettings.java
@@ -0,0 +1,11 @@
+package dev.cd.diagnostics;
+
+public final class DiagnosticsSettings {
+    public static boolean playerEsp = true;
+    public static boolean storageEsp = true;
+    public static boolean tracers = false;
+    public static boolean depthOverride = false;
+
+    private DiagnosticsSettings() {
+    }
+}
